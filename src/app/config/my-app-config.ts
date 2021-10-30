db3717af128742fe03344bfdefe8e9428b066ecb
ChenErdi <chen1886@hotmail.com>
2021-10-30 23:58:34 +0200

    update the status of this project

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa2cz7bbfw4ehqVP5d7',
+        issuer: 'https://dev-32183474.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid','profile', 'email']
+    }
+
+}
---
+++ src/app/services/checkout.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Purchase } from '../common/purchase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+
+
+  private purchaseUrl = environment.eShopApiUrl + '/checkout/purchase';
+
+  constructor(private httpClient: HttpClient) { }
+
+  placeOrder(purchase: Purchase): Observable<Purchase> {
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+}
